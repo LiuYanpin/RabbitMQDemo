48f2b56a4cdbce1cdf82d134d46c8e92d22f9cc0
yanpliu <yanpliu@thoughtworks.com>
2018-12-17 17:26:37 +0800

    PRC todo

---
+++ src/main/java/javaversion/tutorial6/RPCClient.java
@@ -0,0 +1,29 @@
+package javaversion.tutorial6;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class RPCClient implements AutoCloseable {
+
+    private Connection connection;
+    private Channel channel;
+    private String requestQueueName = "rpc_queue";
+
+    public RPCClient() throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+
+        connection = factory.newConnection();
+        channel = connection.createChannel();
+    }
+
+    @Override
+    public void close() throws Exception {
+
+    }
+
+}
